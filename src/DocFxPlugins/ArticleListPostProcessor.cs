1e233bdc73f528c84cec396d98f6afecd3f70d11
JeremyTCD <jeremytanchongdao@gmail.com>
2017-02-13 22:52:49 +0800

    Made major changes to ArticleListPostProcessor
    - Improved function TrimNode. No longer uses static variable to keep track of snippet length. Ends snippets with neater ellipse placement.
    
    - Added function NormalizeSnippet. Converts snippet title into an anchor. Adds no-anchor class to headers (to avoid anchor.js from adding deep links).

--- src/DocFxPlugins/ArticleListPostProcessor.cs
+++ src/DocFxPlugins/ArticleListPostProcessor.cs
@@ -15,9 +15,7 @@ namespace DocFxPlugins
     [Export(nameof(ArticleListPostProcessor), typeof(IPostProcessor))]
     public class ArticleListPostProcessor : IPostProcessor
     {
-        private static readonly Regex RegexWhiteSpace = new Regex(@"\s+", RegexOptions.Compiled);
-        private static readonly int ArticleSnippetMaxLength = 145;
-        private static int ArticleSnippetLength = 0;
+        private int ArticleSnippetLength;
 
         public ImmutableDictionary<string, object> PrepareMetadata(ImmutableDictionary<string, object> metadata)
         {
@@ -35,52 +33,28 @@ namespace DocFxPlugins
                 throw new ArgumentNullException("Base directory cannot be null");
             }
 
-            List<ArticleListItem> articleListItems = GetArticleListItems(manifest);
+            List<ArticleListItem> articleListItems = GetArticleListItems(outputFolder, manifest);
             if (articleListItems.Count == 0)
             {
                 return manifest;
             }
 
             articleListItems.Sort((x, y) => DateTime.Compare(y.Date, x.Date));
-
-            Logger.LogInfo($"Extracting data from {articleListItems.Count} html files");
-            foreach (ArticleListItem articleListItem in articleListItems)
-            {
-                string filePath = Path.Combine(outputFolder, articleListItem.Href);
-                HtmlDocument htmlDoc = new HtmlDocument();
-
-                Logger.LogVerbose($"Extracting data from {filePath}");
-                if (File.Exists(filePath))
-                {
-                    try
-                    {
-                        htmlDoc.Load(filePath, Encoding.UTF8);
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.LogWarning($"Warning: Can't load content from {filePath}: {ex.Message}");
-                        continue;
-                    }
-
-                    ExtractData(htmlDoc, articleListItem);
-                }
-            }
-
-            InsertArticleLists(outputFolder, manifest, articleListItems);
+            InsertArticleListItems(outputFolder, manifest, articleListItems);
 
             return manifest;
         }
 
-        private void InsertArticleLists(string outputFolder, Manifest manifest, List<ArticleListItem> articleListItems)
+        private void InsertArticleListItems(string outputFolder, Manifest manifest, List<ArticleListItem> articleListItems)
         {
             List<string> articleListEnabledFiles = new List<string>();
 
             foreach (ManifestItem manifestItem in manifest.Files)
             {
                 Dictionary<string, object> metadata = manifestItem.Metadata as Dictionary<string, object>;
-                if (metadata != null && metadata.ContainsKey("jr.enableArticleList"))
+                if (metadata != null && metadata.ContainsKey(ArticleListConstants.EnableArticleListKey))
                 {
-                    articleListEnabledFiles.Add(Path.Combine(outputFolder, 
+                    articleListEnabledFiles.Add(Path.Combine(outputFolder,
                         manifestItem.
                             OutputFiles.
                             First(o => o.Key.Equals(".html", StringComparison.OrdinalIgnoreCase)).
@@ -94,81 +68,135 @@ namespace DocFxPlugins
                 HtmlDocument htmlDoc = new HtmlDocument();
                 htmlDoc.Load(file, Encoding.UTF8);
 
-                HtmlNode articleListItemsNode = htmlDoc.DocumentNode.SelectSingleNode("//div[contains(@class, 'al-items')]");
+                HtmlNode articleListItemsNode = htmlDoc.DocumentNode.SelectSingleNode("//div[contains(@class, 'al-items-all')]");
 
                 foreach (ArticleListItem item in articleListItems)
                 {
-                    HtmlNode alItem = htmlDoc.CreateElement("div");
-                    alItem.Attributes.Add("class", "al-item");
-                    articleListItemsNode.AppendChild(alItem);
+                    HtmlNode alItem = HtmlNode.CreateNode("<div class=\"al-item\"></div>");
+                    alItem.AppendChildren(item.Snippet.ChildNodes);
 
-                    alItem.AppendChild(item.Snippet);
+                    articleListItemsNode.AppendChild(alItem);
                 }
 
                 htmlDoc.Save(file);
             }
         }
 
-        private List<ArticleListItem> GetArticleListItems(Manifest manifest)
+        private List<ArticleListItem> GetArticleListItems(string outputFolder, Manifest manifest)
         {
             List<ArticleListItem> articleListItems = new List<ArticleListItem>();
 
-            foreach(ManifestItem manifestItem in manifest.Files)
+            foreach (ManifestItem manifestItem in manifest.Files)
             {
                 Dictionary<string, object> metadata = manifestItem.Metadata as Dictionary<string, object>;
-                if(metadata != null && metadata.ContainsKey("jr.includeInArticleList"))
+                if (metadata == null || !metadata.ContainsKey(ArticleListConstants.IncludeInArticleListKey))
                 {
-                    articleListItems.Add(new ArticleListItem
-                    {
-                        Href = manifestItem.OutputFiles.First(o => o.Key.Equals(".html", StringComparison.OrdinalIgnoreCase)).Value.RelativePath,
-                        Date = (DateTime)metadata["jr.date"]
-
-                    });
+                    continue;
                 }
+
+                string href = manifestItem.OutputFiles.First(o => o.Key.Equals(".html", StringComparison.OrdinalIgnoreCase)).Value.RelativePath;
+                string filePath = Path.Combine(outputFolder, href);
+
+                Logger.LogVerbose($"Generating article list item from {filePath}");
+
+                if (!File.Exists(filePath))
+                {
+                    Logger.LogWarning($"Warning: {filePath} does not exist");
+                }
+
+                HtmlDocument htmlDoc = new HtmlDocument();
+
+                try
+                {
+                    htmlDoc.Load(filePath, Encoding.UTF8);
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogWarning($"Warning: Can't load content from {filePath}: {ex.Message}");
+                    continue;
+                }
+
+                HtmlNode snippet = ExtractSnippet(htmlDoc);
+                if (snippet == null)
+                {
+                    Logger.LogWarning($"Warning: {filePath} has no article node");
+                    continue;
+                }
+                NormalizeSnippet(snippet, href);
+
+                articleListItems.Add(new ArticleListItem
+                {
+                    Href = href,
+                    Snippet = snippet,
+                    Date = (DateTime)metadata[ArticleListConstants.DateKey]
+                });
             }
 
             return articleListItems;
         }
 
-        private void ExtractData(HtmlDocument html, ArticleListItem articleListItem)
+        private HtmlNode ExtractSnippet(HtmlDocument html)
         {
-            StringBuilder contentBuilder = new StringBuilder();
+            HtmlNode snippet = html.DocumentNode.SelectSingleNode("//article");
 
-            // Select content between the data-searchable class tag
-            HtmlNode articleNode = html.DocumentNode.SelectSingleNode("//article");
-
-            if(articleListItem == null)
+            if (snippet == null)
             {
-                throw new InvalidDataException($"{nameof(ArticleListPostProcessor)}: Article {articleListItem.Href} has an empty article");
+                return null;
             }
 
-
-            ArticleSnippetLength = 0;
-            TrimNode(articleNode);
-
-            articleListItem.Snippet = articleNode.Clone();
+            return snippet.Clone();
         }
 
-        private bool TrimNode(HtmlNode node)
+        private void NormalizeSnippet(HtmlNode snippet, string href)
+        {
+            HtmlNode titleAnchorNode = HtmlNode.CreateNode($"<a href=\"/{href}\"></a>");
+            HtmlNode titleNode = snippet.SelectSingleNode(".//h1");
+            titleAnchorNode.InnerHtml = titleNode.InnerText;
+            titleNode.RemoveAllChildren();
+            titleNode.AppendChild(titleAnchorNode);
+
+            TrimNode(snippet, 0);
+
+            HtmlNodeCollection headers = snippet.SelectNodes(".//*[self::h2 or self::h3 or self::h4 or self::h5 or self::h6]");
+            if(headers == null)
+            {
+                return;
+            }
+            foreach (HtmlNode node in headers)
+            {
+                node.Attributes.Add("class", "no-anchor" + node.Attributes["class"]?.Value ?? "");
+            }
+        }
+
+        private int TrimNode(HtmlNode node, int currentSnippetLength)
         {
             if (node.NodeType == HtmlNodeType.Text)
             {
                 string text = node.InnerText.Trim();
-                ArticleSnippetLength += text.Length;
-                int diff = ArticleSnippetMaxLength - ArticleSnippetLength;
+                currentSnippetLength += text.Length;
 
-                if (diff > 0)
+                if (ArticleSnippetLength > currentSnippetLength)
                 {
-                    return true;
+                    return currentSnippetLength;
                 }
-                else if (diff < 0)
+
+                int endIndex = text.IndexOfAny(new char[] { ' ', '.', ',', '!', '?', ';' }, text.Length - (currentSnippetLength - ArticleSnippetLength) - 1);
+
+                if (endIndex == -1)
                 {
-                    node.InnerHtml = text.Substring(0, text.Length + diff);
+                    endIndex = text.Length - 1;
+                }
+
+                node.InnerHtml = text.Substring(0, endIndex + 1);
+
+                if (text[endIndex] != ' ')
+                {
+                    node.InnerHtml += " ";
                 }
 
                 node.InnerHtml += "...";
 
-                return false;
+                return currentSnippetLength;
             }
 
             HtmlNodeCollection childNodes = node.ChildNodes;
@@ -176,8 +204,8 @@ namespace DocFxPlugins
             for (int i = 0; i < childNodes.Count; i++)
             {
                 HtmlNode childNode = childNodes[i];
-
-                if (!TrimNode(childNode))
+                currentSnippetLength = TrimNode(childNode, currentSnippetLength);
+                if (currentSnippetLength >= ArticleSnippetLength)
                 {
                     int numNodesToRemove = childNodes.Count - i - 1;
 
@@ -186,11 +214,11 @@ namespace DocFxPlugins
                         node.RemoveChild(node.LastChild);
                     }
 
-                    return false;
+                    return currentSnippetLength;
                 }
             }
 
-            return true;
-        } 
+            return currentSnippetLength;
+        }
     }
 }
