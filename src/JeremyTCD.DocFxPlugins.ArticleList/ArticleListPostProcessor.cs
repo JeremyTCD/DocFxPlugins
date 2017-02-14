4bdd4039d2e8f7f665cef4f1feffd330f55597ec
JeremyTCD <jeremytanchongdao@gmail.com>
2017-02-14 17:52:31 +0800

    Updated ArticleListPostProcessor to use Shared.SnippetCreator

--- src/JeremyTCD.DocFxPlugins.ArticleList/ArticleListPostProcessor.cs
+++ src/JeremyTCD.DocFxPlugins.ArticleList/ArticleListPostProcessor.cs
@@ -9,6 +9,7 @@ using Microsoft.DocAsCode.Common;
 using HtmlAgilityPack;
 using System.Text;
 using System.Globalization;
+using JeremyTCD.DocFxPlugins.Shared;
 
 namespace JeremyTCD.DocFxPlugins.ArticleList
 {
@@ -120,12 +121,12 @@ namespace JeremyTCD.DocFxPlugins.ArticleList
                     throw new InvalidDataException($"{nameof(ArticleListPostProcessor)}: Article {filePath} cannot be loaded");
                 }
 
-                HtmlNode snippet = htmlDoc.DocumentNode.SelectSingleNode("//article")?.Clone();
-                if (snippet == null)
+                HtmlNode article = htmlDoc.DocumentNode.SelectSingleNode("//article");
+                if (article == null)
                 {
                     throw new InvalidDataException($"{nameof(ArticleListPostProcessor)}: Article {filePath} has no article node");
                 }
-                NormalizeSnippet(snippet, href);
+                HtmlNode snippet = SnippetCreator.CreateSnippet(article, href, ArticleSnippetLength);
 
                 DateTime date = default(DateTime);
                 try
@@ -146,80 +147,6 @@ namespace JeremyTCD.DocFxPlugins.ArticleList
             }
 
             return articleListItems;
-        }
-
-        private void NormalizeSnippet(HtmlNode snippet, string href)
-        {
-            HtmlNode titleAnchorNode = HtmlNode.CreateNode($"<a href=\"/{href}\"></a>");
-            HtmlNode titleNode = snippet.SelectSingleNode(".//h1");
-            titleAnchorNode.InnerHtml = titleNode.InnerText;
-            titleNode.RemoveAllChildren();
-            titleNode.AppendChild(titleAnchorNode);
-
-            TrimNode(snippet, 0);
-
-            HtmlNodeCollection headers = snippet.SelectNodes(".//*[self::h2 or self::h3 or self::h4 or self::h5 or self::h6]");
-            if (headers == null)
-            {
-                return;
-            }
-            foreach (HtmlNode node in headers)
-            {
-                node.Attributes.Add("class", "no-anchor" + node.Attributes["class"]?.Value ?? "");
-            }
-        }
-
-        private int TrimNode(HtmlNode node, int currentSnippetLength)
-        {
-            if (node.NodeType == HtmlNodeType.Text)
-            {
-                string text = node.InnerText.Trim();
-                currentSnippetLength += text.Length;
-
-                if (ArticleSnippetLength > currentSnippetLength)
-                {
-                    return currentSnippetLength;
-                }
-
-                int endIndex = text.IndexOfAny(new char[] { ' ', '.', ',', '!', '?', ';' }, text.Length - (currentSnippetLength - ArticleSnippetLength) - 1);
-
-                if (endIndex == -1)
-                {
-                    endIndex = text.Length - 1;
-                }
-
-                node.InnerHtml = text.Substring(0, endIndex + 1);
-
-                if (text[endIndex] != ' ')
-                {
-                    node.InnerHtml += " ";
-                }
-
-                node.InnerHtml += "...";
-
-                return currentSnippetLength;
-            }
-
-            HtmlNodeCollection childNodes = node.ChildNodes;
-
-            for (int i = 0; i < childNodes.Count; i++)
-            {
-                HtmlNode childNode = childNodes[i];
-                currentSnippetLength = TrimNode(childNode, currentSnippetLength);
-                if (currentSnippetLength >= ArticleSnippetLength)
-                {
-                    int numNodesToRemove = childNodes.Count - i - 1;
-
-                    while (numNodesToRemove-- > 0)
-                    {
-                        node.RemoveChild(node.LastChild);
-                    }
-
-                    return currentSnippetLength;
-                }
-            }
-
-            return currentSnippetLength;
         }
     }
 }
