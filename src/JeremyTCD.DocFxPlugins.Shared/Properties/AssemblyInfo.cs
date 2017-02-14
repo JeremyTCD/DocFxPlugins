64944179981a41e99d40a56b38b83171d203da2e
JeremyTCD <jeremytanchongdao@gmail.com>
2017-02-14 16:10:04 +0800

    Added project JeremyTCD.DocFxPlugins.Shared

---
+++ src/JeremyTCD.DocFxPlugins.Shared/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyProduct("JeremyTCD.DocFxPlugins")]
+[assembly: AssemblyTitle("JeremyTCD.DocFxPlugins.Shared")]
+[assembly: AssemblyCopyright("Copyright © JeremyTCD 2017")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a6eb8c1a-79d3-45bb-8321-2d2852f5f036")]
