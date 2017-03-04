1b7e9434ce1be75f7515831d750501f88299dfa5
JeremyTCD <jeremytanchongdao@gmail.com>
2017-03-04 16:50:42 +0800

    Updated SearchIndexPostProcessor.OutputSearchIndex
    - Changes were made to DocFx's ManifestItem. Accounted for these changes.

--- src/JeremyTCD.DocFxPlugins.SearchIndex/SearchIndexPostProcessor.cs
+++ src/JeremyTCD.DocFxPlugins.SearchIndex/SearchIndexPostProcessor.cs
@@ -58,8 +58,6 @@ namespace JeremyTCD.DocFxPlugins.SearchIndex
             var manifestItem = new ManifestItem
             {
                 DocumentType = "Resource",
-                Metadata = new Dictionary<string, object>(),
-                OutputFiles = new Dictionary<string, OutputFileInfo>()
             };
             manifestItem.OutputFiles.Add("resource", new OutputFileInfo
             {
